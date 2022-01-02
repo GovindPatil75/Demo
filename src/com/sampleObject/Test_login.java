c51db8bedede3d793de0ee745c0fc3fe8672fb1c
GovindPatil75 <mulegr75@gmail.com>
2022-01-02 19:09:41 +0530

    Sample demo commit

---
+++ src/com/sampleObject/Test_login.java
@@ -0,0 +1,10 @@
+package com.sampleObject;
+
+public class Test_login {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Sample Test");
+	}
+
+}
